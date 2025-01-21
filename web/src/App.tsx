e66cffecaf5b3964a15325bd7914db635b65649a
elijah <elijahlucian@gmail.com>
2025-01-21 12:13:11 -0700

    lfg

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,41 @@
 import { Link } from 'react-router-dom'
-import { Divider, FlexCol, H1, P, Page } from './lib'
+import { Divider, FlexCol, FlexRow, Grid, H1, P, Page } from './lib'
 
 export const App = () => {
   return (
-    <Page>
-      <FlexCol
-        justifyContent="center"
-        alignItems="center"
-        height="100vh"
-        gap="1rem"
-      >
-        <H1>elijah lucian</H1>
-        <Divider />
-        <Divider />
-        <Link
-          target="_blank"
-          to="https://open.spotify.com/artist/3DDcytjYeKW2oDq3tWZPVF"
-        >
-          spotify
+    <Grid height="100vh" gridTemplateRows="auto  1fr auto">
+      <FlexRow justifyContent="start" padding="1rem" opacity={0.3}>
+        <Link to="https://nextgenart.elijahlucian.ca">▻ pretty things</Link>
+      </FlexRow>
+      <Page>
+        <FlexCol justifyContent="center" alignItems="center" gap="1rem">
+          <Link to="/">
+            <H1>elijah lucian</H1>
+          </Link>
+          <Divider />
+          <Divider />
+          <Link
+            target="_blank"
+            to="https://open.spotify.com/artist/3DDcytjYeKW2oDq3tWZPVF"
+          >
+            ▻ spotify
+          </Link>
+          <Link target="_blank" to="https://www.instagram.com/elijahlucian">
+            ▻ instagram
+          </Link>
+          <Link target="_blank" to="https://linktr.ee/eli7vh">
+            ▻ linktree
+          </Link>
+        </FlexCol>
+      </Page>
+      <FlexRow justifyContent="end" padding="1rem" opacity={0.3} gap="1rem">
+        <Link target="_blank" to="https://www.patreon.com/c/elijahlucian">
+          ▻ patreon
         </Link>
-        <Link target="_blank" to="https://www.instagram.com/elijahlucian">
-          instagram
+        <Link target="_blank" to="https://github.com/eli7vh/elijahlucian.ca">
+          ▻ source code
         </Link>
-        <Link target="_blank" to="https://linktr.ee/eli7vh">
-          linktree
-        </Link>
-        <Link target="_blank" to="https://github.com/eli7vh">
-          github
-        </Link>
-      </FlexCol>
-    </Page>
+      </FlexRow>
+    </Grid>
   )
 }
--- web/src/lib/components/typography/H1.tsx
+++ web/src/lib/components/typography/H1.tsx
@@ -12,6 +12,8 @@ export const H1 = ({ children, ...style }: Props) => {
         ...style,
         fontFamily: 'wideawake-black',
         letterSpacing: '0.05em',
+        textShadow: '1px 1px 3px #ffffff11, 2px 2px 6px #ffffff55',
+        transition: 'text-shadow 0.3s ease-in-out',
       }}
     >
       {children}
