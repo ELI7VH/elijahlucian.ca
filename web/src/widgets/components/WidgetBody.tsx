f51d898f239d2c8248b6b2ec0a276fdf39e5bb37
elijah <elijahlucian@gmail.com>
2025-02-17 12:31:49 -0700

    widg body

--- web/src/widgets/components/WidgetBody.tsx
+++ web/src/widgets/components/WidgetBody.tsx
@@ -1,4 +1,4 @@
-import { FlexCol, FlexProps, FlexRow } from '@/lib'
+import { Box, FlexCol, FlexProps, FlexRow, Grid } from '@/lib'
 
 type Props = {
   collapsed: boolean
@@ -11,24 +11,33 @@ export const WidgetBody = ({
 }: Props) => {
   return (
     <FlexRow padding="1rem">
-      <FlexCol
-        pointerEvents="all"
-        gap="0.5rem"
-        backgroundColor="var(--brand-1)"
-        opacity={0.5}
-        _hover={{
-          opacity: 1,
-        }}
-        transition="all 0.2s ease-in-out"
-        overflow={collapsed ? 'hidden' : 'visible'}
-        width={collapsed ? '1px' : 'auto'}
-        height={collapsed ? '1px' : 'auto'}
-        borderRadius="1rem"
-        padding={collapsed ? '0' : '1rem'}
-        {...props}
-      >
-        {children}
-      </FlexCol>
+      <Grid>
+        <FlexCol
+          gridArea="1 / 1 / 2 / 2"
+          pointerEvents="all"
+          gap="0.5rem"
+          backgroundColor="var(--brand-1)"
+          // bg="background-image-tex"
+          opacity={0.5}
+          _hover={{
+            opacity: 1,
+          }}
+          transition="all 0.2s ease-in-out"
+          overflow={collapsed ? 'hidden' : 'visible'}
+          width={collapsed ? '1px' : 'auto'}
+          height={collapsed ? '1px' : 'auto'}
+          borderRadius="1rem"
+          padding={collapsed ? '0' : '1rem'}
+          {...props}
+        >
+          {children}
+        </FlexCol>
+        <Box
+          gridArea="1 / 1 / 2 / 2"
+          backgroundImage="var(--background-image-tex)"
+          opacity={0.5}
+        ></Box>
+      </Grid>
     </FlexRow>
   )
 }
