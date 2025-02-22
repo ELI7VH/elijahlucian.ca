dba602afd4d1b55fb4b0a82e727f640c7939f496
elijah <elijahlucian@gmail.com>
2025-02-21 23:03:36 -0700

    NULLL

--- web/src/lib/hooks/useLocalState.ts
+++ web/src/lib/hooks/useLocalState.ts
@@ -9,23 +9,19 @@ export const useLocalState = <T>(key: string, initialValue: T) => {
     const value = localStorage.getItem(key)
     console.log('useLocalState', key, value)
     if (value) {
-      setState(JSON.parse(value || '{}'))
+      setState(JSON.parse(value))
     }
   }, [key])
 
   const set = (value: T) => {
-    // console.log('useLocalState', key, value)
-    if (!value) {
-      rm()
-      return
-    }
+    console.log('useLocalState', key, value)
 
-    localStorage.setItem(key, JSON.stringify(value))
+    localStorage.setItem(key, JSON.stringify(value ?? initialValue))
     setState(value)
   }
 
   const rm = () => {
-    // console.log('useLocalState', key, 'rm')
+    console.log('useLocalState', key, 'rm')
     localStorage.removeItem(key)
     setState(initialValue)
   }
