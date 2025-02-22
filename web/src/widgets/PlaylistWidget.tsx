e8a9e64442b9a017b6851ce64988482e6d8d0dee
elijah <elijahlucian@gmail.com>
2025-02-21 23:25:54 -0700

    local playlists

--- web/src/widgets/PlaylistWidget.tsx
+++ web/src/widgets/PlaylistWidget.tsx
@@ -1,5 +1,7 @@
 import {
+  Box,
   Button,
+  Divider,
   FlexRow,
   H1,
   Input,
@@ -8,7 +10,7 @@ import {
   Table,
   useSearchParams,
 } from '@/lib'
-import { useSongs } from '@/lib/hooks/api/useSongs'
+import { Song, useSongs } from '@/lib/hooks/api/useSongs'
 import { useLocalState } from '@/lib/hooks/useLocalState'
 import { WidgetBadge } from './components/WidgetBadge'
 import { WidgetBody } from './components/WidgetBody'
@@ -22,6 +24,13 @@ export const PlaylistWidget = () => {
   // use localdb to store user state
 
   const starred = useLocalDB<Record<string, boolean>>('starred-songs')
+  const playlists = useLocalDB<Record<string, string[]>>('playlists', {
+    vibes: [],
+    ambient: [],
+    podcasts: [],
+    releases: [],
+  })
+  const playlist = useLocalState('playlist-name', 'vibes')
   const onlyStars = useLocalState('playlist-only-stars', false)
   const sp = useSearchParams()
   const navigate = useNavigate()
@@ -51,6 +60,12 @@ export const PlaylistWidget = () => {
   const prev = () => {
     // if only stars, find prev starred song
   }
+
+  const playlistSongs = playlist.state
+    ? (playlists.value[playlist.state]
+        ?.map((id) => songs.data.find((s) => s.id === id))
+        .filter(Boolean) as Song[]) || []
+    : songs.data
 
   return (
     <WidgetContainer>
@@ -96,9 +111,61 @@ export const PlaylistWidget = () => {
             +
           </Button>
         </FlexRow>
+        <Divider />
+        <FlexRow gap="0.5rem" alignItems="end" justifyContent="space-between">
+          <FlexRow gap="0.5rem">
+            <Button
+              size="small"
+              onClick={() => playlist.set('releases')}
+              variant={playlist.state === 'releases' ? 'contained' : 'text'}
+            >
+              Releases
+            </Button>
+            <Button
+              size="small"
+              onClick={() => playlist.set('podcasts')}
+              variant={playlist.state === 'podcasts' ? 'contained' : 'text'}
+            >
+              Podcasts
+            </Button>
+            <Button
+              size="small"
+              onClick={() => playlist.set('vibes')}
+              variant={playlist.state === 'vibes' ? 'contained' : 'text'}
+            >
+              Vibes
+            </Button>
+
+            <Button
+              size="small"
+              onClick={() => playlist.set('ambient')}
+              variant={playlist.state === 'ambient' ? 'contained' : 'text'}
+            >
+              Ambient
+            </Button>
+            <Button
+              size="small"
+              onClick={() => playlist.set('')}
+              variant="text"
+            >
+              All
+            </Button>
+          </FlexRow>
+          <Button
+            size="small"
+            onClick={() => {
+              playlists.set(playlist.state, [
+                ...(playlists.value[playlist.state] || []),
+                songId,
+              ])
+            }}
+          >
+            +
+          </Button>
+        </FlexRow>
         <Table
           maxHeight="20vh"
-          data={(filtered || songs.data).filter((s) =>
+          data={(filtered || playlistSongs).filter((s) =>
             onlyStars.state ? starred.value[s.id] : true,
           )}
           padding="0.25rem"
@@ -127,6 +194,26 @@ export const PlaylistWidget = () => {
                 width: '100%',
               },
               label: 'Name',
+            },
+            {
+              label: 'Remove',
+              render: (r, i) => (
+                <Button
+                  disabled={!playlist.state}
+                  size="small"
+                  variant="text"
+                  onClick={() => {
+                    playlists.set(
+                      playlist.state,
+                      playlists.value[playlist.state]
+                        .slice(0, i)
+                        .concat(playlists.value[playlist.state].slice(i + 1)),
+                    )
+                  }}
+                >
+                  -
+                </Button>
+              ),
             },
             {
               label: 'Star',
