cb3aac64c7db9c81fa4223f527b5f5778a33f19c
elijah <elijahlucian@gmail.com>
2025-08-04 11:27:39 -0600

    meow

--- web/src/lib/hooks/api/useBaseRecord.ts
+++ web/src/lib/hooks/api/useBaseRecord.ts
@@ -2,24 +2,20 @@ import { useQuery } from '@tanstack/react-query'
 import { useApiContext, useQueryFns } from '../..'
 import { useForm, DefaultValues } from 'react-hook-form'
 
-type BaseRecordProps<T, R> = {
+type BaseRecordProps = {
   id?: string
   path: string
   baseQueryKey?: string[]
-  options?: {
-    onUpdate?: (payload: Partial<R>, response: Partial<T>) => void
-    onDestroy?: (id?: string) => void
-  }
 }
 
 // todo name these types better
 export function useBaseRecord<
   T extends { id: string },
   R extends { id: string } = T,
->(props: BaseRecordProps<T, R>) {
+>(props: BaseRecordProps) {
   const api = useApiContext()
 
-  const fns = useQueryFns<T, Partial<T>, R>({
+  const fns = useQueryFns<T, Partial<T>>({
     queryKey: [props.path, props.id],
     path: props.path,
   })
@@ -31,27 +27,22 @@ export function useBaseRecord<
   })
 
   const form = useForm<T>({
-    defaultValues: (query.data ? {...query.data} : {}) as DefaultValues<T>,
+    defaultValues: (query.data ? { ...query.data } : {}) as DefaultValues<T>,
   })
 
   const update = (data: Partial<T>) =>
     props.id
-      ? fns.update.mutate(
-          { ...data, id: props.id },
-          {
-            onSuccess: (response) => {
-              props.options?.onUpdate?.(response, data)
-              api.client.invalidateQueries({ queryKey: props.baseQueryKey })
-            },
+      ? fns.update.mutate({ ...data, id: props.id } as unknown as Partial<T>, {
+          onSuccess: () => {
+            api.client.invalidateQueries({ queryKey: props.baseQueryKey })
           },
-        )
+        })
       : Promise.reject(`update: No id: ${props.path}`)
 
   const destroy = () =>
     props.id
       ? fns.destroy.mutate(props.id, {
           onSuccess: () => {
-            props.options?.onDestroy?.(props.id)
             api.client.invalidateQueries({ queryKey: props.baseQueryKey })
           },
         })
